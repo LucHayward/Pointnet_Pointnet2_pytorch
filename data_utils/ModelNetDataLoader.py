82b136abaf15e519612d0d625345e0b9a2a2c81e
luc <luchay10@gmail.com>
2021-05-25 20:56:42 +0200

    Continued work on church data loader Added comments

--- data_utils/ModelNetDataLoader.py
+++ data_utils/ModelNetDataLoader.py
@@ -11,7 +11,9 @@ import pickle
 from tqdm import tqdm
 from torch.utils.data import Dataset
 
+from numpy.random import default_rng
 
+rng = default_rng()
 
 warnings.filterwarnings('ignore')
 
@@ -19,7 +21,7 @@ warnings.filterwarnings('ignore')
 def pc_normalize(pc):  # centers all the points around 0, then scales all the points down to between +-1
     centroid = np.mean(pc, axis=0)
     pc = pc - centroid
-    m = np.max(np.sqrt(np.sum(pc**2, axis=1)))
+    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
     pc = pc / m
     return pc
 
@@ -33,7 +35,7 @@ def farthest_point_sample(point, npoint):
         centroids: sampled pointcloud index, [npoint, D]
     """
     N, D = point.shape
-    xyz = point[:,:3]
+    xyz = point[:, :3]
     centroids = np.zeros((npoint,))
     distance = np.ones((N,)) * 1e10
     farthest = np.random.randint(0, N)
@@ -68,7 +70,10 @@ class ModelNetDataLoader(Dataset):
         self.classes = dict(zip(self.cat, range(len(self.cat))))
 
         shape_ids = {}
-        if self.num_category == 10:
+        if self.num_category == 2:
+            shape_ids['train'] = [line.rstrip() for line in open(os.path.join(self.root, 'church_train.txt'))]
+            shape_ids['test'] = [line.rstrip() for line in open(os.path.join(self.root, 'church_test.txt'))]
+        elif self.num_category == 10:
             shape_ids['train'] = [line.rstrip() for line in open(os.path.join(self.root, 'modelnet10_train.txt'))]
             shape_ids['test'] = [line.rstrip() for line in open(os.path.join(self.root, 'modelnet10_test.txt'))]
         else:
@@ -82,7 +87,8 @@ class ModelNetDataLoader(Dataset):
         print('The size of %s data is %d' % (split, len(self.datapath)))
 
         if self.uniform:
-            self.save_path = os.path.join(root, 'modelnet%d_%s_%dpts_fps.dat' % (self.num_category, split, self.npoints))
+            self.save_path = os.path.join(root,
+                                          'modelnet%d_%s_%dpts_fps.dat' % (self.num_category, split, self.npoints))
         else:
             self.save_path = os.path.join(root, 'modelnet%d_%s_%dpts.dat' % (self.num_category, split, self.npoints))
 
@@ -120,8 +126,8 @@ class ModelNetDataLoader(Dataset):
         if self.process_data:
             point_set, label = self.list_of_points[index], self.list_of_labels[index]
         else:
-            fn = self.datapath[index]
-            cls = self.classes[self.datapath[index][0]]
+            fn = self.datapath[index]  # filename
+            cls = self.classes[self.datapath[index][0]]  # class
             label = np.array([cls]).astype(np.int32)
             point_set = np.loadtxt(fn[1], delimiter=',').astype(np.float32)
 
@@ -129,7 +135,11 @@ class ModelNetDataLoader(Dataset):
                 point_set = farthest_point_sample(point_set, self.npoints)
             else:
                 point_set = point_set[0:self.npoints, :]
-                
+                # TODO No need, rather use uniform/fps
+                # TODO Sample self.npoints random points without repeat, could use this to reindex/shuffle the array?
+                # random_idxs = rng.choice(np.arange(0, len(point_set)),self.npoints, replace=False)
+                # point_set = point_set[random_idxs]
+
         point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
         if not self.use_normals:  # For modelnet the last three are normals, not RGB
             point_set = point_set[:, 0:3]
