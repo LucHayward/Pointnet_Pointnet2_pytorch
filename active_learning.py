061b0a1e64f743c893f4df581ad9b48c906ae6f8
Luc Hayward <luchay10@gmail.com>
2022-05-23 23:33:41 +0200

    Start work on a baseline get_diverse_cells()

--- active_learning.py
+++ active_learning.py
@@ -65,40 +65,63 @@ def get_high_variance_cells(cell_variance, point_variances, num_cells, grid_mask
     """
     Choose K segments for labelling by the user (probably the same number as initial selection*3)
     Uses the variance of the cells
-    :return:
+    :return: the cells for the high variance points, and the points
     """
     var_cutoff = np.sort(cell_variance)[::-1][num_cells]
     high_var_points_idxs = np.where(point_variances >= var_cutoff)[0]
     num_grid_cells = len(np.unique(grid_mask))
-    num_high_var_cells = len(np.unique(grid_mask[high_var_points_idxs]))
+    high_var_cells = np.unique(grid_mask[high_var_points_idxs])
+    num_high_var_cells = len(high_var_cells)
     # num_high_var_cells = len(np.unique(val_old_ds.grid_mask[high_var_points_idxs]))
     print(f"Selected high variance cells (mean variance >= {var_cutoff:.2f}), "
           f"{len(high_var_points_idxs)}/{len(point_variances)} points ({len(high_var_points_idxs) / len(point_variances) * 100:.2f}%),"
-          f"{num_high_var_cells}/{num_grid_cells} cells ({num_high_var_cells / num_grid_cells*100:.2f}%)")
+          f"{num_high_var_cells}/{num_grid_cells} cells ({num_high_var_cells / num_grid_cells * 100:.2f}%)")
+
+    return high_var_cells, high_var_points_idxs
+    # return high_var_cells
 
 
-def get_diverse_cells():
-    pass
+def get_diverse_cells(cells, point_idxs, points, grid_mask):
+    """
+    Given a set of cells (expected high variance set) calculates a diverse subset
+    Diversity is computed as the distance measure of the cell from the average of the cells
+    TODO could be based on the distance measure of the cell from the average cell computed in feature space
+    :return: The cells with high diversity
+    """
+    from scipy.spatial import distance
+    average_cell_position = np.mean(points, axis=0)
+
+    distances = []
+    for cell in cells:
+        cell_point_idxs = np.where(grid_mask == cell)
+        cell_average = np.mean(points(cell_point_idxs))
+        distances.append((cell, distance.sqeuclidean(average_cell_position, cell_average)))
+
+    distances = np.array(distances)
+    distances.sort()
+    # TODO get the top N cells, calculate the point_idxs for those cells and return the cells and the point_idxs
+
+    return None
 
 
 def main():
     # get full pcd
-    cache_initial_dataset = Path("data/PatrickData/Church/MastersFormat/cache_full_dataset.pickle")
-    initial_dataset = None
-    if cache_initial_dataset.exists():
-        with open(cache_initial_dataset, "rb") as cache_file:
-            initial_dataset = pickle.load(cache_file)
-    else:
-        initial_dataset = MastersDataset(None, Path("data/PatrickData/Church/MastersFormat"), sample_all_points=True)
-        with open(cache_initial_dataset, "wb") as cache_file:
-            pickle.dump(initial_dataset, cache_file)
-
-    v_init = Visualisation_utils.pptk_full_dataset(initial_dataset, include_grid_mask=True, include_intensity=True)
-    v_init.color_map(
-        Visualisation_utils.turbo_colormap_data[::16] * 16)  # Repeats colours distinguishing adjacent cells.
-    v_init.color_map("summer")  # Best for intensity which is all we have to work with.
-
-    selected_labelled_idxs, selected_cells = select_new_points_to_label(initial_dataset, v_init)
+    # cache_initial_dataset = Path("data/PatrickData/Church/MastersFormat/cache_full_dataset.pickle")
+    # initial_dataset = None
+    # if cache_initial_dataset.exists():
+    #     with open(cache_initial_dataset, "rb") as cache_file:
+    #         initial_dataset = pickle.load(cache_file)
+    # else:
+    #     initial_dataset = MastersDataset(None, Path("data/PatrickData/Church/MastersFormat"), sample_all_points=True)
+    #     with open(cache_initial_dataset, "wb") as cache_file:
+    #         pickle.dump(initial_dataset, cache_file)
+    #
+    # v_init = Visualisation_utils.pptk_full_dataset(initial_dataset, include_grid_mask=True, include_intensity=True)
+    # v_init.color_map(
+    #     Visualisation_utils.turbo_colormap_data[::16] * 16)  # Repeats colours distinguishing adjacent cells.
+    # v_init.color_map("summer")  # Best for intensity which is all we have to work with.
+    #
+    # selected_labelled_idxs, selected_cells = select_new_points_to_label(initial_dataset, v_init)
     AL_iteration = 0
     # save_split_dataset(initial_dataset, selected_labelled_idxs, AL_iteration)
     # del initial_dataset
@@ -117,9 +140,9 @@ def main():
 
     # train_args.npoint *= 4
     # train_args.batch_size = 8
-    train_args.validate_only = True
+    # train_args.validate_only = True
 
-    train_masters.main(train_args)
+    # train_masters.main(train_args)
 
     #   Now we need the predictions from the last good trained model (which we saved in the training)
     with np.load(LOG_DIR / 'train' / 'val_predictions.npz') as npz_file:
@@ -129,15 +152,15 @@ def main():
         predict_variance = npz_file['variance']
         predict_point_variance = npz_file['point_variance']
         predict_grid_mask = npz_file['predict_grid_mask']  # 388 cells temp
-    print()
 
     # Show merged pointcloud in pptk
     # val_old_ds = MastersDataset('validate', LOG_DIR / str(AL_iteration), sample_all_points=True)
     v = pptk.viewer(predict_points[:, :3], predict_points[:, -1], predict_preds, predict_target, predict_point_variance)
     v.color_map('summer')
     selected_cells = [i for i in range(10)]
-    get_high_variance_cells(predict_variance, predict_point_variance, len(selected_cells) * 3, predict_grid_mask)
-    get_diverse_cells()
+    high_var_cells, high_var_point_idxs = get_high_variance_cells(predict_variance, predict_point_variance,
+                                                                  len(selected_cells) * 3, predict_grid_mask)
+    chosen_cells = get_diverse_cells(high_var_cells, high_var_point_idxs, predict_points, predict_grid_mask)
 
 
 if __name__ == '__main__':
