5a69ce0febfc842f7499191a96882afb03e859d5
Luc Hayward <luchay10@gmail.com>
2022-06-04 12:10:34 +0200

    Working on diversity

--- active_learning.py
+++ active_learning.py
@@ -11,7 +11,6 @@ import Visualisation_utils
 import train_masters
 from data_utils.MastersDataset import MastersDataset
 
-TEMP_DIR = Path("temp/")
 LOG_DIR = Path("log/active_learning")
 
 
@@ -79,8 +78,9 @@ def get_high_variance_cells(cell_variance, point_variances, num_cells, grid_mask
     # return high_var_cells
 
 
-def get_diverse_cells(cells, point_idxs, points, grid_mask, features):
+def get_diverse_cells_by_distance(cells, point_idxs, points, grid_mask, features):
     """
+    NOT_YET_IMPLEMENTED
     Given a set of cells (expected high variance set) calculates a diverse subset
     Diversity is computed as the distance measure of the cell from the average of the cells
     TODO could be based on the distance measure of the cell from the average cell computed in feature space
@@ -90,9 +90,6 @@ def get_diverse_cells(cells, point_idxs, points, grid_mask, features):
     from scipy.spatial import distance
 
     distances = distance.pdist(features, 'sqeuclidean')
-
-
-
     average_cell_position = np.mean(points, axis=0)
 
     distances = []
@@ -106,6 +103,39 @@ def get_diverse_cells(cells, point_idxs, points, grid_mask, features):
     # TODO get the top N cells, calculate the point_idxs for those cells and return the cells and the point_idxs
 
     return None
+
+def get_diversity_ranking(features, variance, n_clusters=10, penalty_factor=0.9):
+    """
+    Score each sample with its uncertainty U
+    Clusters the samples into K clusters based on their feature embeddings
+    Sort the regions based on the uncertainty U
+    For each region, penalise the scores of the remaining regions in that cluster by some factor P
+    The result is a ranking of regions based on uncertainty and diversity
+    (such that the most uncertain regions are ranked first, but repeat regions from the same cluster are unlikely).
+    """
+    from sklearn import cluster
+    variance_ordering_idxs = variance.argsort()[::-1]
+    kmeans = cluster.KMeans(n_clusters=n_clusters, random_state=0).fit(features)
+
+    print(F"Debug: Initial ordering:")
+    for idx in variance_ordering_idxs[:20]:
+        print(f"Idx {idx}, Cluster {kmeans.labels_[idx]},  variance {variance[idx]:.4f} ")
+
+    adjusted_variance = np.copy(variance)
+    for i, idx in enumerate(variance_ordering_idxs):  # iterate over the clusters in order of variance
+        current_cluster = kmeans.labels_[idx]
+        for x in range(i + 1, len(variance_ordering_idxs)):  # iterate over the remaining clusters in order of variance
+            x = variance_ordering_idxs[x]
+            if kmeans.labels_[x] == current_cluster:  # Scale the variances in the same cluster
+                adjusted_variance[x] *= penalty_factor
+
+    print(f"Old variance_ordering_idxs:\n{list(zip(variance_ordering_idxs[:10], kmeans.labels_[:10]))}")
+    adjusted_variance_ordering_idxs = adjusted_variance.argsort()[::-1]
+    print(f"New variance_ordering_idxs:\n{list(zip(adjusted_variance_ordering_idxs[:10], kmeans.labels_[variance_ordering_idxs[:10]]))}")
+    for idx in adjusted_variance_ordering_idxs[:20]:
+        print(f"Idx {idx}, Cluster {kmeans.labels_[idx]},  variance {adjusted_variance[idx]:.4f}")
+
+    return adjusted_variance_ordering_idxs, kmeans.labels_
 
 
 def main():
@@ -155,19 +185,28 @@ def main():
         predict_target = npz_file['target']
         predict_variance = npz_file['variance']  # Variances are normalised to [-1,1]
         predict_point_variance = npz_file['point_variance']
-        predict_grid_mask = npz_file['grid_mask']
+        predict_grid_mask = npz_file['grid_mask'].astype('int16')
         predict_features = npz_file['features']
         predict_samples_per_cell = npz_file['samples_per_cell']
 
         # Show merged pointcloud in pptk
     # val_old_ds = MastersDataset('validate', LOG_DIR / str(AL_iteration), sample_all_points=True)
-    v = pptk.viewer(predict_points[:, :3], predict_points[:, -1], predict_preds, predict_target, predict_point_variance)
+    v = pptk.viewer(predict_points[:, :3], predict_points[:, -1], predict_preds, predict_target, predict_point_variance, predict_grid_mask)
     v.color_map('summer')
-    selected_cells = [i for i in range(10)]
-    high_var_cells, high_var_point_idxs = get_high_variance_cells(predict_variance, predict_point_variance,
-                                                                  len(selected_cells) * 3, predict_grid_mask)
-    diverse_cells = get_diverse_cells(high_var_cells, high_var_point_idxs, predict_points, predict_grid_mask, predict_features[high_var_cells])
+    num_cells = 10  # Not sure how many we need, maybe more?
+    selected_cells = [i for i in range(num_cells)]
+    # high_var_cells, high_var_point_idxs = get_high_variance_cells(predict_variance, predict_point_variance,
+    #                                                               len(selected_cells) * 3, predict_grid_mask)
+    # diverse_cells = get_diverse_cells_by_distance(high_var_cells, high_var_point_idxs, predict_points, predict_grid_mask, predict_features[high_var_cells])
 
+    adjusted_variance_ordering_idxs, cluster_labels = get_diversity_ranking(predict_features, predict_variance, num_cells)
+    new_point_idxs = np.where(np.in1d(predict_grid_mask, adjusted_variance_ordering_idxs[:num_cells]))[0]
+    v_new = pptk.viewer(predict_points[new_point_idxs, :3], predict_points[new_point_idxs, -1], predict_preds[new_point_idxs], predict_target[new_point_idxs], predict_point_variance[new_point_idxs], predict_grid_mask[new_point_idxs])
+    v.set(selected=new_point_idxs)
+
+
+
+    print()
 
 if __name__ == '__main__':
     import os
